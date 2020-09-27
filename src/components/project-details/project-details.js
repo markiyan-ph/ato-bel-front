6fd7789a3925145e65d4c319ae4c357ee373e751
Markiyan Keleman <markiyan.ph@gmail.com>
2020-09-27 19:32:45 +0300

    Project details retriving project data

--- src/components/project-details/project-details.js
+++ src/components/project-details/project-details.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect} from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import Content from "../content";
+import * as actions from "../../store/actions";
 
 import "./project-details.scss";
 
 const ProjectsDetails = (props) => {
   const id = props.match.params.id;
-  
+  const projects = useSelector(state => state.projects);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(actions.fetchProjectById(id));
+    
+    // eslint-disable-next-line
+  }, [id]);
+
+  const project = projects.projectsList[0] ?? {};
+
   return (
     <Content classNames={"flex-child"}>
-      <h1>Hello Project Details! Your Id is {id}</h1>
+      <div className="project-details">
+        <h1>Hello Project Details! Your Id is {id}</h1>
+        ${JSON.stringify(project)}
+      </div>
     </Content>
   );
 };
