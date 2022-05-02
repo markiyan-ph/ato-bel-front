cc87e6d3ffac78e10c8f7d2be3b5b8473824290a
Markiyan <markiyan.ph@gmail.com>
2022-05-02 15:45:50 +0200

    Switch deprecated createStore to configureStore

--- src/index.js
+++ src/index.js
@@ -2,31 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { applyMiddleware, combineReducers, compose, createStore } from "redux";
-import createSagaMiddleware from "redux-saga";
+import { configureStore } from "@reduxjs/toolkit";
 import App from "./components/app/";
-import "./i18n";
-import "./index.scss";
+import createSagaMiddleware from "redux-saga";
 import { projectsReducer, projectDataReducer } from "./store/reducers";
 import { watchProjects } from './store/sagas/index';
 
-const composeEnhancers =
-  // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "development" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+import "./i18n";
+import "./index.scss";
 
-const rootReducer = combineReducers({
+const rootReducer = {
   projects: projectsReducer,
   projectData: projectDataReducer
-});
+};
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({thunk: false}).concat(sagaMiddleware),
+  devTools: process.env.NODE_ENV !== 'production'
+});
 
 sagaMiddleware.run(watchProjects);
 
