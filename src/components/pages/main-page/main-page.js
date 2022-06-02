73f212ef3ba4b3a517350fec14a7d8d44ec1b258
Markiyan <markiyan.ph@gmail.com>
2022-06-02 16:01:29 +0200

    Rewrite slider using hooks. Remove not needed logic

--- src/components/pages/main-page/main-page.js
+++ src/components/pages/main-page/main-page.js
@@ -1,102 +1,29 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../../../store/actions";
 import MSlider from "../../slider";
-import {
-  getRandomInt,
-  getIndexById,
-  reorderList
-} from "../../../tools/helpers";
 import "./main-page.scss";
 
-class NewMainPage extends Component {
-  state = {
-    projectId: null,
-    loadProjects: false,
-    showControlButtons: false
-  };
+const MainPage = () => {
+  const dispatch = useDispatch();
+  const {
+    projects: { mainPageProjects },
+  } = useSelector(state => state);
+  
+  const fetchMainPageProjects = () => dispatch(actions.fetchMainPageProjects());
+  const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-  componentDidMount() {
-    const {
-      projects: { mainPageProjects },
-      onFetchRandomProject
-    } = this.props;
+  useEffect(() => {
+    fetchMainPageProjects();
+  }, [mainPageProjects.length]);
 
-    if (mainPageProjects.length === 0) {
-      onFetchRandomProject();
-      this.setState({ loadProjects: true });
-    } else {
-      const randProject = getRandomInt(0, mainPageProjects.length);
-      this.setState(({ projectId }) => {
-        const id = projectId ? projectId : mainPageProjects[randProject]._id;
-        return { projectId: id };
-      });
-    }
-  }
+  const initialSlide = randomInt(0, mainPageProjects.length-1);
 
-  componentDidUpdate(prevState) {
-    const {
-      projects: { mainPageProjects },
-    } = this.props;
-
-    const { loadProjects } = this.state;
-
-    if (prevState.projects.mainPageProjects.length !== mainPageProjects.length) {
-      if (mainPageProjects.length === 1 && loadProjects) {
-        this.setState({ projectId: mainPageProjects[0]._id });
-      }
-    }
-  }
-
-  imgOnload = (loaded) => {
-    const { loadProjects } = this.state;
-    const {
-      projects: { mainPageProjects },
-    } = this.props;
-    const { onFetchProjects } = this.props;
-
-    if (loaded && loadProjects) {
-      onFetchProjects(); 
-      this.setState({ loadProjects: false });
-    } else if(loaded && mainPageProjects.length > 1) {
-      this.setState({ showControlButtons: true });
-    }
-  };
-
-  render() {
-    const {
-      projects: { loading, mainPageProjects, error },
-    } = this.props;
-    const { projectId, showControlButtons } = this.state;
-    const slideIndex = getIndexById(mainPageProjects, projectId);
-
-    const orderedProjectList = reorderList(mainPageProjects, slideIndex);
-
-    if (error) {
-      return <h5>Just error.</h5>;
-    }
-
-    const content =
-      !loading && projectId ? (
-        <MSlider projects={orderedProjectList} imgOnload={this.imgOnload} showButtons={showControlButtons} />
-      ) : (
-        <p>Loading... </p>
-      );
-
-    return <div className="main-page">{content}</div>;
-  }
-}
-const mapStateToProps = state => {
-  return {
-    projects: state.projects
-  };
+  const content = mainPageProjects.length > 0 ? 
+    (<MSlider projects={mainPageProjects} showButtons={true} initialSlide={initialSlide} />)
+    : <p>Loading...</p>;
+  
+  return content;
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onFetchProjects: () => dispatch(actions.fetchMainPageProjects()),
-    onFetchRandomProject: () => dispatch(actions.fetchRandomProject())
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(NewMainPage);
+export default MainPage;
--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useRef, useState } from "react";
 import Slider from "react-slick";
 import SliderBar from "./slider-bar";
 import { getServerAPI } from "../../tools/helpers";
@@ -8,83 +8,80 @@ import { isMobile } from '../../tools/helpers';
 import "./slider.scss";
 
 const CustomSlide = props => {
-  const { imgSrc, title, imgOnload } = props;
+  const { imgSrc, title } = props;
 
   return (
     <div>
       <img
-        // src={`data:image/png;base64,${imgSrc}`}
         src={`${getServerAPI()}/uploads${imgSrc}`}
         alt={title}
-        onLoad={() => imgOnload('done')}
       />
     </div>
   );
 };
 
-export default class MSlider extends Component {
-  state = {
-    title: this.props.projects[0].title
+function MSlider ({projects, showButtons, initialSlide}) {
+  const [title, setTitle] = useState(`${projects[initialSlide].title}`);
+  const slider = useRef(null);
+
+  const next = () => {
+    slider.current.slickNext();
   };
 
-  next = () => {
-    this.slider.slickNext();
+  const previous = () => {
+    slider.current.slickPrev();
+  };
+  
+  const settings = {
+    // accessibility: true,
+    dots: false,
+    // fade: true,
+    infinite: true,
+    speed: 500,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+    // lazyLoad: true,
+    pauseOnHover: false,
+    // cssEase: "linear",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    touchMove: isMobile(),
+    initialSlide,
+    // focusOnSelect: true,
+    beforeChange: (_oldIndex, newIndex) => {
+      setTitle(projects[newIndex].title);
+    }
   };
 
-  previous = () => {
-    this.slider.slickPrev();
-  };
+  const slides = projects.map(({ _id, imgSrc, title }) => (
+    <CustomSlide key={_id} imgSrc={imgSrc} title={title} />
+  ));
 
-  render() {
-    const { projects, imgOnload, showButtons } = this.props;
-
-    const settings = {
-      accessibility: true,
-      dots: false,
-      // fade: true,
-      infinite: true,
-      speed: 500,
-      // autoplay: true,
-      // autoplaySpeed: 3000,
-      pauseOnHover: false,
-      // cssEase: "linear",
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      touchMove: isMobile(),
-      focusOnSelect: true,
-      beforeChange: (_oldIndex, newIndex) => {
-        this.setState({ title: this.props.projects[newIndex].title });
-      }
-    };
-
-    const slides = projects.map(({ _id, imgSrc, title }) => (
-      <CustomSlide key={_id} imgSrc={imgSrc} title={title} imgOnload={imgOnload} />
-    ));
-
-    return (
-      <div
-        className="slider"
-        onKeyDown={(e) => {
-          if (window.event && window.event.keyCode === 9) {
-            e.preventDefault();
-            return false;
-          }
-        }}
+  return (
+    <div
+      className="slider"
+      onKeyDown={(e) => {
+        if (window.event && window.event.keyCode === 9) {
+          e.preventDefault();
+          return false;
+        }
+      }}
+    >
+      <Slider
+        style={{ overflow: "hidden" }}
+        ref={slider}
+        {...settings}
       >
-        <Slider
-          style={{ overflow: "hidden" }}
-          ref={c => (this.slider = c)}
-          {...settings}
-        >
-          {slides}
-        </Slider>
-        <SliderBar
-          prev={this.previous}
-          next={this.next}
-          showButtons={showButtons}
-          title={this.state.title}
-        />
-      </div>
-    );
-  }
+        {slides}
+      </Slider>
+      <SliderBar
+        prev={previous}
+        next={next}
+        showButtons={showButtons}
+        title={title}
+      />
+    </div>
+  );
 }
+
+export default MSlider;
