02ea84db0b3d39335ece37d591b3d810c2ae8e7c
Markiyan <markiyan.ph@gmail.com>
2022-08-18 16:19:46 +0200

    Remove footer and wrapper from contacts component

--- src/components/pages/contacts/contacts.js
+++ src/components/pages/contacts/contacts.js
@@ -1,33 +1,26 @@
-import React from "react";
-import Content from "../../content";
-import Footer from "../../footer";
+import React from 'react';
+// import Footer from '../../footer';
 
-import "./contacts.scss";
+import './contacts.scss';
 
 const Contacts = () => {
   return (
     <div className="wrapper">
-      <Content classNames={"flex-child"}>
-        <div className="contacts d-flex flex-column align-items-start justify-content-center">
-          <h3>Проектна Майстерня Бєляєвих</h3>
-          <br />
-          <h5>м. Львів, вул. Алли Горської 5а</h5>
-          <h5>79000, Україна</h5>
-          <h5>
-            <a href="tel:+380638326925">+38 063 8326925</a>
-          </h5>
-          <h5>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="mailto:office@ato-bel.com.ua"
-            >
-              office@ato-bel.com.ua
-            </a>
-          </h5>
-        </div>
-      </Content>
-      <Footer />
+      <div className="contacts d-flex flex-column align-items-center justify-content-center">
+        <h3>Проектна Майстерня Бєляєвих</h3>
+        <br />
+        <h5>м. Львів, вул. Алли Горської 5а</h5>
+        <h5>79000, Україна</h5>
+        <h5>
+          <a href="tel:+380638326925">+38 063 8326925</a>
+        </h5>
+        <h5>
+          <a target="_blank" rel="noopener noreferrer" href="mailto:office@ato-bel.com.ua">
+            office@ato-bel.com.ua
+          </a>
+        </h5>
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 };
