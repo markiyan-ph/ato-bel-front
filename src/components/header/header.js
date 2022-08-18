a5e236cd69700204d53b4277c876f2c19e8c49b9
Markiyan <markiyan.ph@gmail.com>
2022-08-18 11:53:54 +0200

    Reformatted header code

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { Link, NavLink, useLocation } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import React, { useState } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { Instagram, Facebook, Behance } from '../social-icons';
 
-import "./header.scss";
-import logo from "../../assets/images/atoBelLogo.svg";
+import './header.scss';
+import logo from '../../assets/images/atoBelLogo.svg';
 
-const LinkMenu = ({ content, title, link, click, className='' }) => {
+const LinkMenu = ({ content, title, link, click, className = '' }) => {
   content = content ?? title;
   return (
     <Link to={link} onClick={click} key={title} className={className}>
@@ -31,7 +31,7 @@ const Header = () => {
   const { pathname } = useLocation();
 
   // lang "uk" or "en"
-  const switchLanguage = (lang) => {
+  const switchLanguage = lang => {
     if (lang !== language) {
       i18n.changeLanguage(lang);
       setLanguage(lang);
@@ -39,80 +39,64 @@ const Header = () => {
   };
 
   const menuItems = [
-    { title: t("header.menu.projects"), link: "/projects/", click: null },
-    { title: t("header.menu.studio"), link: "/studio/", click: null },
+    { title: t('header.menu.projects'), link: '/projects/', click: null },
+    { title: t('header.menu.studio'), link: '/studio/', click: null },
     // { title: t("header.menu.blog"), link: "/blog/", click: null },
-    { title: t("header.menu.contacts"), link: "/contacts/", click: null }
+    { title: t('header.menu.contacts'), link: '/contacts/', click: null },
   ];
 
   const menu = menuItems.map(({ title, link, click }) => {
     return (
       <li key={title}>
-        <NavLink
-          className={({isActive}) => (isActive ? "is-active" : "")}
-          to={link}
-          onClick={click}
-          title={title}
-        >
+        <NavLink className={({ isActive }) => (isActive ? 'is-active' : '')} to={link} onClick={click} title={title}>
           {title}
         </NavLink>
       </li>
     );
   });
 
-  
   const listOfSocialNetworks = [
-    {icon: <Facebook />, link: 'https://www.facebook.com/atobeldesign', title: 'facebook-icon'}, 
-    {icon: <Instagram />, link: 'https://www.instagram.com/ato_bel_architects/', title: 'instagram-icon'}, 
-    {icon: <Behance />, link: 'https://www.behance.net/ATO-Bel', title: 'behance-icon'}
+    {
+      icon: <Facebook />,
+      link: 'https://www.facebook.com/atobeldesign',
+      title: 'facebook-icon',
+    },
+    {
+      icon: <Instagram />,
+      link: 'https://www.instagram.com/ato_bel_architects/',
+      title: 'instagram-icon',
+    },
+    {
+      icon: <Behance />,
+      link: 'https://www.behance.net/ATO-Bel',
+      title: 'behance-icon',
+    },
   ];
-  const socialNetworks = listOfSocialNetworks.map(({icon, link, title}) => {
+  const socialNetworks = listOfSocialNetworks.map(({ icon, link, title }) => {
     return (
       <a href={link} key={title}>
         {icon}
       </a>
-    );});
-    
+    );
+  });
 
   return (
     <header className="header d-flex align-items-center">
       <div className="logo-container">
-        <LinkMenu
-          content={<Logo />}
-          title="company-logo"
-          link={"/"}
-          click={null}
-          className="home-link mr-auto"
-        />
+        <LinkMenu content={<Logo />} title="company-logo" link={'/'} click={null} className="home-link mr-auto" />
       </div>
-      
-      <div className={`menu ${(pathname === '/' || pathname ===  '/newMain/') ? '' : 'underscore'}`}>
-        <ul className="d-flex align-items-center">
-          {menu}
-        </ul>
+
+      <div className={`menu ${pathname === '/' || pathname === '/newMain/' ? '' : 'underscore'}`}>
+        <ul className="d-flex align-items-center">{menu}</ul>
       </div>
-      
+
       <div className="language-container d-flex align-items-center">
         <div className="d-flex align-items-center">
-          <LinkMenu
-            content="UA"
-            title="UA"
-            link={"#"}
-            className={language === 'uk' ? "is-active" : ""}
-            click={() => switchLanguage('uk')}
-          /> &nbsp;/&nbsp;
-          <LinkMenu
-            content="EN"
-            title="EN"
-            link={"#"}
-            className={language === 'en' ? "is-active" : ""}
-            click={() => switchLanguage('en')}
-          />
+          <LinkMenu content="UA" title="UA" link={'#'} className={language === 'uk' ? 'is-active' : ''} click={() => switchLanguage('uk')} /> &nbsp;/&nbsp;
+          <LinkMenu content="EN" title="EN" link={'#'} className={language === 'en' ? 'is-active' : ''} click={() => switchLanguage('en')} />
         </div>
 
-        <div className="social-networks d-flex align-items-center">
-          {socialNetworks}
-        </div>
+        <div className="social-networks d-flex align-items-center">{socialNetworks}</div>
       </div>
     </header>
   );
