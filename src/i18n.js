7449db7bf2f430fdd4baf1ce9caa4fb71c03b799
Markiyan <markiyan.ph@gmail.com>
2022-09-07 00:59:35 +0200

    Detect and save selected language

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,5 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 // import Backend from 'i18next-xhr-backend';
@@ -8,21 +9,26 @@ import resources from './locales';
 // have a look at the Quick start guide 
 // for passing in lng and translations on init
 
+window.userLang = navigator.language || navigator.userLanguage;
+
+let language = window.userLang.split('-')[0];
+const FALLBACK_LOCALE = 'en';
+
 i18n
   // load translation using xhr -> see /public/locales
   // learn more: https://github.com/i18next/i18next-xhr-backend
   // .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  // .use(LanguageDetector)
+  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    lng: 'en',
-    // lng: 'uk',
+    lng: localStorage.getItem('i18nextLng') ?? language,
+    fallbackLng: FALLBACK_LOCALE,
     debug: false,
 
     interpolation: {
