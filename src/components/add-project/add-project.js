697db40d7f5e8af8fb2720a1364e384f4f57ee44
Markiyan <markiyan.ph@gmail.com>
2022-09-07 12:25:14 +0200

    increase modal size

--- src/components/add-project/add-project.js
+++ src/components/add-project/add-project.js
@@ -5,7 +5,7 @@ import AddProjectForm from './add-project-form';
 const AddProject = ({show, modalClose}) => {
   
   return (
-    <Modal show={show} onHide={modalClose}>
+    <Modal size='lg' show={show} onHide={modalClose}>
       <Modal.Body>
         <AddProjectForm />
       </Modal.Body>
