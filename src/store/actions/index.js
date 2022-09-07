e68cce35d3ba8739f1ca9cfacd1867a492d4bad5
Markiyan <markiyan.ph@gmail.com>
2022-09-07 11:08:49 +0200

    Improve exports

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,25 +1,5 @@
-export { 
-  fetchProjects,
-  fetchProjectsLoading,
-  fetchMainPageProjects,
-  fetchProjectsSuccess,
-  fetchMainPageProjectsSuccess,
-  fetchProjectsFail,
-} from './projects';
+export * from './projects';
 
-export {
-  fetchProjectDetails,
-  fetchProjectDetailsSuccess,
-  projectDetailsLoading,
-  saveProjectDetails,
-  saveProjectDetailsSuccess
-} from './project-data';
+export * from './project-data';
 
-export {
-  authorizeUser,
-  authorizeUserSuccess,
-  authorizeUserFail,
-  unAuthorizeUser,
-  unAuthorizeUserSuccess,
-  unAuthorizeUserFail
-} from './authorization';
+export * from './authorization';
