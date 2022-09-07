7f72d3b5ebdbb5c7fffa8c42bc8e8fa6225c3191
Markiyan <markiyan.ph@gmail.com>
2022-09-07 10:54:20 +0200

    Change quotes

--- src/components/pages/main-page/main-page.js
+++ src/components/pages/main-page/main-page.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import * as actions from "../../../store/actions";
-import MSlider from "../../slider";
-import "./main-page.scss";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../../../store/actions';
+import MSlider from '../../slider';
+import './main-page.scss';
 
 const MainPage = () => {
   const dispatch = useDispatch();
   const {
     projects: { mainPageProjects },
   } = useSelector(state => state);
-  
+
   const fetchMainPageProjects = () => dispatch(actions.fetchMainPageProjects());
   const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
@@ -17,19 +17,19 @@ const MainPage = () => {
     fetchMainPageProjects();
   }, [mainPageProjects.length]);
 
-  const initialSlide = randomInt(0, mainPageProjects.length-1);
+  const initialSlide = randomInt(0, mainPageProjects.length - 1);
 
-  const content = mainPageProjects.length > 0 ? 
-    (<MSlider 
-      projects={
-        mainPageProjects.map(
-          project => ({...project, imgSrc: `${project.projectId}/project-main-img/${project.image}`})
-        )} 
-      showButtons={true} 
-      initialSlide={initialSlide}
-    />)
-    : <p>Loading...</p>;
-  
+  const content =
+    mainPageProjects.length > 0 ? (
+      <MSlider
+        projects={mainPageProjects.map(project => ({ ...project, imgSrc: `${project.projectId}/project-main-img/${project.image}` }))}
+        showButtons={true}
+        initialSlide={initialSlide}
+      />
+    ) : (
+      <p>Loading...</p>
+    );
+
   return content;
 };
 
