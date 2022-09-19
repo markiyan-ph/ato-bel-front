4e80124a65aed60fd08de6191efc6eda2311d448
Markiyan <markiyan.ph@gmail.com>
2022-09-19 09:37:52 +0200

    Add titles to modal forms

--- src/components/modal-forms/modal-forms.js
+++ src/components/modal-forms/modal-forms.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Modal, ModalBody } from 'react-bootstrap';
+import { Modal, ModalBody, ModalHeader, ModalTitle } from 'react-bootstrap';
 
-const ModalForm = ({show, modalClose, form}) => {
+const ModalForm = ({show, modalClose, form, formTitle = ''}) => {
   
   return (
     <Modal size='lg' show={show} onHide={modalClose}>
+      <ModalHeader closeButton>
+        <ModalTitle>{formTitle}</ModalTitle>
+      </ModalHeader>
       <ModalBody>
         {form}
       </ModalBody>
