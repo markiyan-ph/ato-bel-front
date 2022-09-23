949c1c4a77fda9d2fed8fad1e9382a313bab9efa
Markiyan <markiyan.ph@gmail.com>
2022-09-23 14:20:45 +0200

    Use select plugin to select tags

--- src/components/modal-forms/add-project-form/add-project-form.js
+++ src/components/modal-forms/add-project-form/add-project-form.js
@@ -1,14 +1,56 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
+import Select from 'react-select';
 
 import './add-project-form.scss';
 
+const options = [
+  { value: '1', label: '1' },
+  { value: '2', label: '2' },
+  { value: '3', label: '3' },
+  { value: '4', label: '4' },
+  { value: '5', label: '5' },
+];
+
+const color = '#ffffff';
+const backgroundColor = '#0d6efd';
+// const backgroundColor = '#01bd07';
+// const crosHover = '#0386ce';
+// const crosHover = '#077cbd';
+const crosHover = '#01bd07';
+
+const colourStyles = {
+  // dropdownIndicator: styles => ({
+  //   ...styles,
+  //   color: '#000000',
+  // }),
+  multiValue: styles => ({
+    ...styles,
+    backgroundColor: backgroundColor,
+  }),
+  multiValueLabel: styles => ({
+    ...styles,
+    color: color,
+  }),
+  multiValueRemove: styles => ({
+    ...styles,
+    color: color,
+    ':hover': {
+      backgroundColor: crosHover,
+      color: color,
+    },
+  }),
+};
+
 const AddProjectForm = () => {
+  // const [field, setField] = useState([]);
+  // console.log(field);
+
   return (
-    <Form className='add-project-form'>
+    <div className="add-project-form">
       <Form.Group className="mb-3" controlId="titles">
         <Form.Label>Project title</Form.Label>
-        <div className='inline-input'>
+        <div className="inline-input">
           <Form.Control type="text" placeholder="Enter english title" />
           <Form.Control type="text" placeholder="Enter ukraine title" />
         </div>
@@ -19,15 +61,27 @@ const AddProjectForm = () => {
 
       <Form.Group className="mb-3" controlId="formFile">
         <Form.Label>Project image</Form.Label>
-        <Form.Control type="file" accept='image/*' />
+        <Form.Control type="file" accept="image/*" />
       </Form.Group>
       {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group> */}
+
+      <Form.Group className="mb-3" controlId="formFile">
+        <Form.Label>Select react tags</Form.Label>
+        <Select
+          closeMenuOnSelect={false}
+          // onChange
+          // defaultValue={[colourOptions[4], colourOptions[5]]}
+          styles={colourStyles}
+          isMulti
+          options={options}
+        />
+      </Form.Group>
       <Button variant="primary" type="submit">
         Submit
       </Button>
-    </Form>
+    </div>
   );
 };
 
