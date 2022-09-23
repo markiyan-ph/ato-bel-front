93692a270dd3fc4fe8bcc2a390bbeeb7870a0f81
Markiyan <markiyan.ph@gmail.com>
2022-09-23 14:20:26 +0200

    Don't close the box when click outsize the model window

--- src/components/modal-forms/modal-forms.js
+++ src/components/modal-forms/modal-forms.js
@@ -4,7 +4,7 @@ import { Modal, ModalBody, ModalHeader, ModalTitle } from 'react-bootstrap';
 const ModalForm = ({show, modalClose, form, formTitle = ''}) => {
   
   return (
-    <Modal size='lg' show={show} onHide={modalClose}>
+    <Modal size='lg' backdrop='static' show={show} onHide={modalClose} >
       <ModalHeader closeButton>
         <ModalTitle>{formTitle}</ModalTitle>
       </ModalHeader>
