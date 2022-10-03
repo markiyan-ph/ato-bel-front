a3f3aad33eb796c6aa875462d4d1dd08567d560a
Markiyan <markiyan.ph@gmail.com>
2022-10-03 17:35:33 +0200

    Validate date and upload fields after submit

--- src/components/modal-forms/add-project-form/add-project-form.js
+++ src/components/modal-forms/add-project-form/add-project-form.js
@@ -42,6 +42,7 @@ const AddProjectForm = ({showModal}) => {
   const dispatch = useDispatch();
   const [projectDate, setProjectDate] = useState(new Date().toISOString().split('T')[0]);
   const [projectImage, setProjectImage] = useState(null);
+  const [checkValidation, setCheckValidation] = useState(false);
   const [projectTitleUk, setProjectTitleUk] = useState('');
   const [projectTitleEn, setProjectTitleEn] = useState('');
   const [projectSubtitleUk, setProjectSubtitleUk] = useState('');
@@ -54,7 +55,9 @@ const AddProjectForm = ({showModal}) => {
 
   const addProject = formData => dispatch(actions.addProject(formData));
   const handleProjectDateChange = e => setProjectDate(e.target.value);
-  const handleProjectImageChange = e => setProjectImage(e.target.files[0]);
+  const handleProjectImageChange = e => {
+    setProjectImage(e.target.files[0]);
+  };
   const handleProjectTitleUkChange = e => setProjectTitleUk(e.target.value);
   const handleProjectTitleEnChange = e => setProjectTitleEn(e.target.value);
   const handleProjectSubtitleUkChange = e => setProjectSubtitleUk(e.target.value);
@@ -62,17 +65,24 @@ const AddProjectForm = ({showModal}) => {
   const handleProjectTagsChange = e => setProjectTags(e);
   
   const handleSubmit = () => {
-    const formData = new FormData();
-    const formDataTags = JSON.stringify(projectTags.length > 0 ? projectTags.map(opt => opt.value) : []);
-    formData.append('project-image', projectImage);
-    formData.append('project-name-uk', projectTitleUk);
-    formData.append('project-name-en', projectTitleEn);
-    formData.append('project-subtitle-uk', projectSubtitleUk);
-    formData.append('project-subtitle-en', projectSubtitleEn);
-    formData.append('project-date', projectDate);
-    formData.append('project-tags', formDataTags);
-    addProject(formData);
-    showModal(false);
+    if (projectImage === null || notValidProjectDate) {
+      setCheckValidation(true);
+    } else {
+      const formData = new FormData();
+      const formDataTags = JSON.stringify(projectTags.length > 0 ? projectTags.map(opt => opt.value) : []);
+      
+      formData.append('project-image', projectImage);
+      formData.append('project-name-uk', projectTitleUk);
+      formData.append('project-name-en', projectTitleEn);
+      formData.append('project-subtitle-uk', projectSubtitleUk);
+      formData.append('project-subtitle-en', projectSubtitleEn);
+      formData.append('project-date', projectDate);
+      formData.append('project-tags', formDataTags);
+      
+      addProject(formData);
+      showModal(false);
+    }
+    
   };
   const notValidProjectDate = !regex.test(projectDate);
 
@@ -96,12 +106,13 @@ const AddProjectForm = ({showModal}) => {
 
       <Form.Group className="mb-3" controlId="formFile">
         <Form.Label>Project image</Form.Label>
-        <Form.Control type="file" accept="image/*" onChange={handleProjectImageChange} />
+        <Form.Control type="file" accept="image/*" onChange={handleProjectImageChange} isInvalid={checkValidation ? projectImage === null : false} />
+        <Form.Control.Feedback type="invalid">Please select an image</Form.Control.Feedback>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="tagId">
         <Form.Label>Project date</Form.Label>
-        <Form.Control type="input" value={projectDate} onChange={handleProjectDateChange} isInvalid={notValidProjectDate} />
+        <Form.Control type="input" value={projectDate} onChange={handleProjectDateChange} isInvalid={checkValidation ? notValidProjectDate : false} />
         <Form.Control.Feedback type="invalid">Please enter valid date</Form.Control.Feedback>
       </Form.Group>
 
