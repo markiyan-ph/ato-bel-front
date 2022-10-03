2378baeecfd59f197d9ddbeea351db3be4fd9d96
Markiyan <markiyan.ph@gmail.com>
2022-10-03 02:50:40 +0200

    Add property projectId to EditTool

--- src/components/gallery/edit-tool/edit-tool.js
+++ src/components/gallery/edit-tool/edit-tool.js
@@ -3,32 +3,38 @@ import { Trash, Pencil, Image } from 'react-bootstrap-icons';
 
 import './edit-tool.scss';
 
-const EditTool = () => {
-  
-  const editClick = (e) => {
+const EditTool = ({ projectId }) => {
+  const editClick = e => {
     e.stopPropagation();
     e.preventDefault();
     console.log('=== Edit project ===');
   };
 
-  const mainPageClick = (e) => {
+  const mainPageClick = e => {
     e.stopPropagation();
     e.preventDefault();
     console.log('=== Add project to main page ===');
   };
-  
-  const deleteClick = (e) => {
+
+  const deleteClick = e => {
     e.stopPropagation();
     e.preventDefault();
     console.log('=== Delete project ===');
+    console.log('projectId', projectId);
   };
-  
+
   return (
     <div className="edit-tools-wrapper">
       <div className="edit-tools">
-        <div className="edit" onClick={(e) => editClick(e)}><Pencil /></div>
-        <div className="add-main-page" onClick={(e) => mainPageClick(e)}><Image /></div>
-        <div className="delete" onClick={(e) => deleteClick(e)}><Trash /></div>
+        <div className="edit" onClick={e => editClick(e)}>
+          <Pencil />
+        </div>
+        <div className="add-main-page" onClick={e => mainPageClick(e)}>
+          <Image />
+        </div>
+        <div className="delete" onClick={e => deleteClick(e)}>
+          <Trash />
+        </div>
       </div>
     </div>
   );
