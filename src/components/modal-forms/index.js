9fd9b4360aa9fe654fd4c0edd71a50fb9be3f57a
Markiyan <markiyan.ph@gmail.com>
2022-10-05 00:42:04 +0200

    Export Update form

--- src/components/modal-forms/index.js
+++ src/components/modal-forms/index.js
@@ -1,9 +1,11 @@
-import ModalForm from "./modal-forms";
+import ModalForm from './modal-forms';
 import AddProjectForm from './add-project-form';
 import TagsForm from './tags-form';
+import UpdateProjectForm from './update-project-form';
 
 export {
   AddProjectForm,
   ModalForm,
-  TagsForm
+  TagsForm,
+  UpdateProjectForm
 };
