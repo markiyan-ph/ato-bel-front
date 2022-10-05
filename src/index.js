7e9c9d31ba434bf7ef4cb0b12c4b04b15136f30a
Markiyan <markiyan.ph@gmail.com>
2022-10-06 01:01:49 +0200

    Do not serialize update calls

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     thunk: false, 
     serializableCheck: {
-      ignoredActions: [actionTypes.ADD_PROJECT]
+      ignoredActions: [actionTypes.ADD_PROJECT, actionTypes.UPDATE_PROJECT]
     }
   }).concat(sagaMiddleware),
   devTools: process.env.NODE_ENV !== 'production'
