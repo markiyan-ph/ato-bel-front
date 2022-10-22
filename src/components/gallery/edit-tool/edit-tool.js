f3d6eb22bae2ef9525ae5f7b56c2791db1427b27
Markiyan <markiyan.ph@gmail.com>
2022-10-22 22:53:41 +0200

    Add main project image functionality

--- src/components/gallery/edit-tool/edit-tool.js
+++ src/components/gallery/edit-tool/edit-tool.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Trash, Pencil, Image } from 'react-bootstrap-icons';
 import { useDispatch } from 'react-redux';
-import { ModalForm, UpdateProjectForm } from '../../modal-forms';
+import { ModalForm, UpdateProjectForm, AddToMainPageForm } from '../../modal-forms';
 import * as actions from '../../../store/actions';
 
 import './edit-tool.scss';
@@ -10,15 +10,19 @@ import ConfirmationForm from '../../modal-forms/confirmation-form';
 const EditTool = ({ projectId }) => {
   const dispatch = useDispatch();
   const [showUpdateForm, popupUpdate] = useState(false);
+  const [showMainPageImageForm, popupMainPageImage] = useState(false);
   const [showDeleteConfirmation, popupDeleteConfirmation] = useState(false);
   const openUpdate = () => popupUpdate(true);
   const closeUpdate = () => popupUpdate(false);
+  const openMainPageImage = () => popupMainPageImage(true);
+  const closeMainPageImage = () => popupMainPageImage(false);
   const openConfirmation = () => popupDeleteConfirmation(true);
   const closeConfirmation = () => popupDeleteConfirmation(false);
 
   const deleteProject = projectId => dispatch(actions.deleteProject(projectId));
 
   const updateProjectForm = <UpdateProjectForm showModal={popupUpdate} projectId={projectId} />;
+  const updateMainPageProjectForm = <AddToMainPageForm showModal={popupMainPageImage} projectId={projectId} />;
 
   const editClick = e => {
     e.stopPropagation();
@@ -31,6 +35,7 @@ const EditTool = ({ projectId }) => {
     e.stopPropagation();
     e.preventDefault();
     console.log('+++ Add project to main page +++');
+    openMainPageImage();
   };
   
   const deleteClick = e => {
@@ -55,9 +60,14 @@ const EditTool = ({ projectId }) => {
           <Pencil />
         </div>
         <div className="add-main-page" onClick={e => mainPageClick(e)}>
-          {/* <div onClick={e => e.stopPropagation()}>
-            <ModalForm show={showUpdateForm} modalClose={closeUpdate} form={updateProjectForm} formTitle="Update project data" />
-          </div> */}
+          <div onClick={e => e.stopPropagation()}>
+            <ModalForm
+              show={showMainPageImageForm}
+              modalClose={closeMainPageImage}
+              form={updateMainPageProjectForm}
+              formTitle="Update project data"
+            />
+          </div>
           <Image />
         </div>
         <div className="delete" onClick={e => deleteClick(e)}>
--- src/components/modal-forms/index.js
+++ src/components/modal-forms/index.js
@@ -2,9 +2,11 @@ import ModalForm from './modal-forms';
 import AddProjectForm from './add-project-form';
 import TagsForm from './tags-form';
 import UpdateProjectForm from './update-project-form';
+import AddToMainPageForm from './add-to-main-page-form';
 
 export {
   AddProjectForm,
+  AddToMainPageForm,
   ModalForm,
   TagsForm,
   UpdateProjectForm
