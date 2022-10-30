53daced1364651d40c5bdb890231e6f2d5dbad04
Markiyan <markiyan.ph@gmail.com>
2022-10-30 23:19:37 +0100

    Split project details component to controller and templates

--- src/components/project-details/project-details.js
+++ src/components/project-details/project-details.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { ArrowUpShort } from 'react-bootstrap-icons';
 
-import Content from '../content';
-import './project-details.scss';
+import { FullWidthTemplate } from './templates';
 
 const projectDetailsObj = {
   detailMainImage: 'http://localhost:5005/uploads/63581e7be801e095c85b4d5b/project-main-img/MG_HEADER_1920x587.jpeg',
@@ -52,62 +50,7 @@ const projectDetailsObj = {
 console.log(projectDetailsObj);
 
 const ProjectsDetails = () => {
-  const backToTopClick = (e) => {
-    e.preventDefault();
-    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-  };
-
-  const specificationsNames = projectDetailsObj.projectInfo.specifications.map(specification => (
-    <div key={specification._id+'name'}>
-      {specification.name}
-    </div>
-  ));
-  
-  const specificationsValues = projectDetailsObj.projectInfo.specifications.map(specification => (
-    <div key={specification._id+'value'}>
-      {specification.value}
-    </div>
-  ));
-
-  const images = projectDetailsObj.images.map(imageData => (
-    <div className="image-data-container" key={imageData._id}>
-      <div className="image">
-        <img src={imageData.img} alt={imageData.imgTitle} />
-      </div>
-      <div className="image-data">
-        <div className="image-title">{imageData.imgTitle}</div>
-        <div className="image-description">{imageData.imgDescription}</div>
-      </div>
-    </div>
-  ));
-
-  return (
-    <Content classNames={'flex-child'}>
-      <div className="project-details">
-        <div className="main-image">
-          <img src={projectDetailsObj.detailMainImage} alt={projectDetailsObj.projectInfo.title} />
-        </div>
-        <div className="project-info">
-          <div className="project-title">
-            <h4>{projectDetailsObj.projectInfo.title}</h4>
-          </div>
-          <div className="project-text">
-            <span>{projectDetailsObj.projectInfo.text}</span>
-          </div>
-          <div className="project-specifications">
-            <div className="specification-name">{specificationsNames}</div>
-            <div className="specification-value">{specificationsValues}</div>
-          </div>
-        </div>
-        <div className="images">
-          {images}
-        </div>
-        
-        <div className='back-to-top' onClick={backToTopClick}><ArrowUpShort /><span className='back-to-top-text'>back to top</span></div>
-      </div>
-      
-    </Content>
-  );
+  return <FullWidthTemplate projectDetailsObj={projectDetailsObj} />;
 };
 
 export default ProjectsDetails;
