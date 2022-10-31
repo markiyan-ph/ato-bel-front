8cce83d3da43ab55f32f9b678e5ee1e75ade298c
Markiyan <markiyan.ph@gmail.com>
2022-10-31 23:52:47 +0100

    remove hardcoded host

--- src/components/project-details/project-details.js
+++ src/components/project-details/project-details.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { FullWidthTemplate } from './templates';
 
 const projectDetailsObj = {
-  detailMainImage: 'http://localhost:5005/uploads/63581e7be801e095c85b4d5b/project-main-img/MG_HEADER_1920x587.jpeg',
+  detailMainImage: '/uploads/63581e7be801e095c85b4d5b/project-main-img/MG_HEADER_1920x587.jpeg',
   projectInfo: {
     title: 'Villa Magnolia',
     text: 'The project is located in Lviv (Ukraine), in the south-eastern district of the city, just 7 km away from the city center. The designed area is part of a mixed-use urban block, that needs both densification and intensification. It is directly adjacent to the major city road from the South and surrounded by now degraded green area of former cooperative gardens from the other sides.',
@@ -33,14 +33,14 @@ const projectDetailsObj = {
   images: [
     {
       _id: Date.now(),
-      img: 'http://localhost:5005/uploads/63581e7be801e095c85b4d5b/project-img/b051b5b7eb61becdeaae058b1015d412',
+      img: '/uploads/63581e7be801e095c85b4d5b/project-img/b051b5b7eb61becdeaae058b1015d412',
       imgTitle: 'Magnolia day light',
       imgDescription:
         'Cupidatat aliquip commodo deserunt minim ad ut veniam eu ipsum fugiat minim. Fugiat do velit magna laboris adipisicing dolor excepteur sunt in do esse consequat. Ullamco cupidatat laborum duis minim nisi adipisicing anim ullamco in ullamco magna sit dolore aliquip. Voluptate nulla ea ex velit proident.',
     },
     {
       _id: Date.now(),
-      img: 'http://localhost:5005/uploads/633aeab5b5da11ddf089bbce/project-img/e80ef1e6427656e4a83e847f2f10f349',
+      img: '/uploads/633aeab5b5da11ddf089bbce/project-img/e80ef1e6427656e4a83e847f2f10f349',
       imgTitle: 'Not Magnolia at all',
       imgDescription: '',
     },
