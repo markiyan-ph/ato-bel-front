75672479dcc106c278db3fa7d72300e81b5d24fb
Markiyan <markiyan.ph@gmail.com>
2022-11-12 13:45:51 +0100

    Added support for multilanguage and added edit elements

--- src/components/project-details/project-details.js
+++ src/components/project-details/project-details.js
@@ -1,60 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import * as actions from '../../store/actions';
 import { FullWidthTemplate } from './templates';
 
-const projectDetailsObj = {
-  detailMainImage: '/uploads/63581e7be801e095c85b4d5b/project-main-img/MG_HEADER_1920x587.jpeg',
-  projectInfo: {
-    title: 'Villa Magnolia',
-    text: 'The project is located in Lviv (Ukraine), in the south-eastern district of the city, just 7 km away from the city center. The designed area is part of a mixed-use urban block, that needs both densification and intensification. It is directly adjacent to the major city road from the South and surrounded by now degraded green area of former cooperative gardens from the other sides.',
-    specifications: [
-      {
-        _id: Date.now(),
-        name: 'Location',
-        value: 'Lviv, Ukraine',
-      },
-      {
-        _id: Date.now(),
-        name: 'Year',
-        value: '2016',
-      },
-      {
-        _id: Date.now(),
-        name: 'Area',
-        value: '0,000 m²',
-      },
-      {
-        _id: Date.now(),
-        name: 'Client',
-        value: 'Vash Dim',
-      },
-    ],
-  },
-  images: [
-    {
-      _id: Date.now(),
-      img: '/uploads/63581e7be801e095c85b4d5b/project-img/b051b5b7eb61becdeaae058b1015d412',
-      imgTitle: 'Magnolia day light',
-      imgDescription:
-        'Cupidatat aliquip commodo deserunt minim ad ut veniam eu ipsum fugiat minim. Fugiat do velit magna laboris adipisicing dolor excepteur sunt in do esse consequat. Ullamco cupidatat laborum duis minim nisi adipisicing anim ullamco in ullamco magna sit dolore aliquip. Voluptate nulla ea ex velit proident.',
-    },
-    {
-      _id: Date.now(),
-      img: '/uploads/633aeab5b5da11ddf089bbce/project-img/e80ef1e6427656e4a83e847f2f10f349',
-      imgTitle: 'Not Magnolia at all',
-      imgDescription: '',
-    },
-  ],
-};
-
-console.log(projectDetailsObj);
-
 const ProjectsDetails = () => {
+  const {i18n} = useTranslation();
+  const lang = i18n.language;
   const { projectId } = useParams();
   const dispatch = useDispatch();
   const projectDetails = useSelector(state => state.projectDetails);
+  const {isAdmin, isAuthorized} = useSelector(state => state.authorization);
   // const {isAdmin, isAuthorized} = useSelector(state => state.authorization);
 
   console.log(projectDetails);
@@ -63,12 +20,14 @@ const ProjectsDetails = () => {
     dispatch(actions.fetchProjectDetails(projectId));
   };
 
+  const showControls = isAdmin && isAuthorized;
+
   useEffect(() => {
     fetchProjectDetails(projectId);
   }, [projectId]);
 
   // return <FullWidthTemplate projectDetailsObj={projectDetailsObj} />;
-  return <FullWidthTemplate projectId={projectId} projectDetailsObj={projectDetails.details} />;
+  return <FullWidthTemplate projectId={projectId} projectDetailsObj={projectDetails.details} isAdmin={showControls} language={lang} />;
 };
 
 export default ProjectsDetails;
--- src/components/project-details/templates/full-width.js
+++ src/components/project-details/templates/full-width.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ArrowUpShort } from 'react-bootstrap-icons';
+import DetailEditTool from '../detail-edit-tool';
 
 import Content from '../../content';
 import './full-width.scss';
+import { Button } from 'react-bootstrap';
 
-const FullWidthTemplate = ({ projectId, projectDetailsObj }) => {
+const FullWidthTemplate = ({ projectId, projectDetailsObj, language, isAdmin }) => {
   const detailImgsPath = `/uploads/${projectId}/detail-imgs`;
   
   const backToTopClick = (e) => {
@@ -14,45 +16,55 @@ const FullWidthTemplate = ({ projectId, projectDetailsObj }) => {
 
   const specificationsNames = projectDetailsObj.projectInfo.specifications.map(specification => (
     <div key={specification._id+'name'}>
-      {specification.name}
+      {specification.name[language]}
     </div>
   ));
   
   const specificationsValues = projectDetailsObj.projectInfo.specifications.map(specification => (
     <div key={specification._id+'value'}>
-      {specification.value}
+      {specification.value[language]}
     </div>
   ));
 
   const images = projectDetailsObj.images.map(imageData => (
     <div className="image-data-container" key={imageData._id}>
       <div className="image">
-        <img src={`${detailImgsPath}/${imageData.img}` } alt={imageData.imgTitle} loading='lazy' />
+        {isAdmin ? <DetailEditTool /> : null}
+        <img src={`${detailImgsPath}/${imageData.img}` } alt={imageData?.imgTitle?.[language]} loading='lazy' />
       </div>
       <div className="image-data">
-        <div className="image-title">{imageData.imgTitle}</div>
-        <div className="image-description">{imageData.imgDescription}</div>
+        <div className="image-title">{imageData?.imgTitle?.[language]}</div>
+        <div className="image-description">{imageData?.imgDescription?.[language]}</div>
       </div>
     </div>
   ));
+
+  const EditProjectInfoButton = () => (
+    <div className="edit-project-info-button">
+      <Button>Edit</Button>
+    </div>
+  );
 
   return (
     <Content classNames={'flex-child'}>
       <div className="project-details">
         <div className="main-image">
+          {isAdmin ? <DetailEditTool /> : null}
           <img src={`${detailImgsPath}/${projectDetailsObj.detailMainImage}`} alt={projectDetailsObj.projectInfo.title} loading='lazy' />
         </div>
         <div className="project-info">
           <div className="project-title">
-            <h4>{projectDetailsObj.projectInfo.title}</h4>
+            <h4>{projectDetailsObj.projectInfo.title[language]}</h4>
           </div>
           <div className="project-text">
-            <span>{projectDetailsObj.projectInfo.text}</span>
+            <span>{projectDetailsObj.projectInfo.text[language]}</span>
           </div>
           <div className="project-specifications">
             <div className="specification-name">{specificationsNames}</div>
             <div className="specification-value">{specificationsValues}</div>
           </div>
+
+          {isAdmin ? <EditProjectInfoButton /> : null}
         </div>
         <div className="images">
           {images}
