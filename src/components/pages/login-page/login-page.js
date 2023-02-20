9cccc261599674afa523e84ffc5224dfa675a98c
Markiyan <markiyan.ph@gmail.com>
2023-02-21 00:36:30 +0100

    Authorization error messages

--- src/components/pages/login-page/login-page.js
+++ src/components/pages/login-page/login-page.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-// import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
-import {} from 'react';
+import { Message } from '../../message';
+import * as actions from '../../../store/actions';
 import './login-page.scss';
 
 const LoginPage = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
+  const [loginForm, setLoginForm] = useState({
+    userName: '',
+    userPassword: '',
+  });
   const from = location.state?.from?.pathname || '/';
   // const storageKey = localStorage.getItem('adminToken');
   // const { authorization } = useSelector(state => state);
@@ -18,8 +24,15 @@ const LoginPage = () => {
   //   }
   // }, [storageKey]);
 
+  const handleInputChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setLoginForm({ ...loginForm, [name]: value });
+  };
+
   const handleLoginSubmit = e => {
     e.preventDefault();
+    dispatch(actions.authorizeUser(loginForm.userName, loginForm.userPassword));
     navigate(from, { replace: true });
   };
 
@@ -28,7 +41,13 @@ const LoginPage = () => {
       <Form className="login-page-form" onSubmit={handleLoginSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>User</Form.Label>
-          <Form.Control type="text" placeholder="Enter user" />
+          <Form.Control
+            type="text"
+            value={loginForm.userName}
+            name="userName"
+            onChange={handleInputChange}
+            placeholder="Username"
+          />
           {/* <Form.Text className="text-muted">
           We&apos;ll never share your email with anyone else.
         </Form.Text> */}
@@ -36,7 +55,13 @@ const LoginPage = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            value={loginForm.userPassword}
+            name="userPassword"
+            onChange={handleInputChange}
+            placeholder="Password"
+          />
         </Form.Group>
         {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
@@ -45,6 +70,7 @@ const LoginPage = () => {
           Submit
         </Button>
       </Form>
+      <Message showMessage={false} variant='danger'>Test new message Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quae nemo suscipit voluptate non delectus architecto quia temporibus eveniet! Modi atque autem asperiores soluta rerum vero ipsa dicta temporibus saepe.</Message>
     </div>
   );
 };
