1b96f2c44bfd84bbcf8a9e0e20b77a77b23c688c
Markiyan <markiyan.ph@gmail.com>
2023-02-21 12:50:54 +0100

    Cleanup message when message disappear

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Alert } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 import './message.scss';
 
 // Acceptable variants ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark']
-const Message = ({ children, showMessage, variant }) => {
+const Message = ({ children, showMessage, cleanupAction, variant='danger' }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => () => dispatch(cleanupAction()), []);
+
   return (
     <div className={`message-wrapper ${showMessage ? 'show-message' : ''}`}>
       <Alert variant={variant}>{children}</Alert>
