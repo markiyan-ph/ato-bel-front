816bcc7246d5d20a591e7723447875d0ffcaed9c
markiyan-ph <markiyan.ph@gmail.com>
2023-09-06 00:54:47 +0200

    Addeded tag activation logic

--- src/components/header/sub-menu/sub-menu.js
+++ src/components/header/sub-menu/sub-menu.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useTranslation } from "react-i18next";
-import { hideHeaderSubMenu } from '../../../tools';
+import { useTranslation } from 'react-i18next';
+import { hideHeaderSubMenu, updateDeleteListItem } from '../../../tools';
 import * as actions from '../../../store/actions';
 
 import './sub-menu.scss';
@@ -17,27 +17,40 @@ import './sub-menu.scss';
 const SubHeader = ({ pathname }) => {
   const dispatch = useDispatch();
   const tags = useSelector(state => state.tags.tagsList);
-  const {i18n} = useTranslation();
+  const { i18n } = useTranslation();
   const lang = i18n.language;
-  
+  const [selectedTags, setSelectedTags] = useState([]);
+
   let menu = [];
   let menuList = [];
-  
+
   useEffect(() => {
     dispatch(actions.fetchTags());
     // if (pathname === '/projects/') {
     //   if (tags.length === 0) {
     //   }
-  
+
     //   menuList = tags;
     // }
   }, [tags.length]);
 
   if (pathname === '/projects/') {
     menuList = tags;
-    menu = menuList.map(({tagId, labels}) => (
-      <li key = {tagId}>
-        <a title = {tagId} className = "" href = '#'>
+    menu = menuList.map(({ tagId, labels }) => (
+      <li key={tagId}>
+        <a
+          title={tagId}
+          className={selectedTags.includes(tagId) ? 'is-active' : ''}
+          // TODO: move onClick to separate function
+          onClick={e => {
+            e.preventDefault();
+            setSelectedTags(selectedTags =>
+              selectedTags.includes(tagId)
+                ? updateDeleteListItem(selectedTags, selectedTags.indexOf(tagId))
+                : [...selectedTags, tagId]
+            );
+          }}
+        >
           {labels[lang]}
         </a>
       </li>
@@ -50,10 +63,10 @@ const SubHeader = ({ pathname }) => {
       { title: 'maps', link: 'https://www.google.com.ua/maps' },
       { title: 'github', link: 'https://github.com/' },
     ];
-    
-    menuList.map(({title, link}) => (
-      <li key = {title}>
-        <a title = {title} className = "" href = {link}>
+
+    menuList.map(({ title, link }) => (
+      <li key={title}>
+        <a title={title} className="" href={link}>
           {title}
         </a>
       </li>
